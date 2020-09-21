9746701d872d5db8464c7b36d67f34b29e9bb45b
fabri7-1 <71575285+fabri7-1@users.noreply.github.com>
2020-09-20 20:33:33 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,18 @@
+//https://www.eclipse.org/paho/clients/js/
+
+
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-  
+
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "skillet776@yahoo.com",
-    password: "1993orellana768",
+    userName: "iatorres@hotmail.es",
+    password: "israel123torres",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -20,12 +23,11 @@
   // called when the client connects
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
-    console.log("Conectado...");
-	
-    client.subscribe("skillet776@yahoo.com/test");
-    message = new Paho.MQTT.Message("Conectado al servidor NTP");
-    message.destinationName = "skillet776@yahoo.com/test1";
-    client.send(message);
+    console.log("Conectado.....");
+    client.subscribe("iatorres@hotmail.es/test");
+	message=new Paho.MQTT.Message("Servidor NTP");
+	message.destinationName=("iatorres@hotmail.es/test1");
+	client.send(message);
 	
   }
 
@@ -38,18 +40,19 @@
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
-      
     }
   }
 
   // called when a message arrives
   function onMessageArrived(message) {
     Mensaje=message.payloadString;
-    console.log(Mensaje);
-    S1=document.getElementById("sensor")
-    S2=document.getElementById("sensor1")
-    S3=document.getElementById("sensor2")
-    S1.innerHTML=" "+Mensaje.split(":")[0];
-    S2.innerHTML=" "+Mensaje.split(":")[1];
-    S3.innerHTML=" "+Mensaje.split(":")[2];
+	console.log(Mensaje);
+	 S1=document.getElementById("Sensor")
+	 S2=document.getElementById("Sensor1")
+	 S3=document.getElementById("Sensor2")
+	 S1.innerHTML="Hora:"+Mensaje.split(".")[0];
+     S2.innerHTML="Minutos:"+Mensaje.split(".")[1];
+	 S3.innerHTML="Segundos:"+Mensaje.split(".")[2];
   }
+  }
+
